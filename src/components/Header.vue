c2706c8b004abe578db59a1864ab5f755a817914
DFKorn <d1man2192@mail.ru>
2025-11-28 20:53:43 +0300

    create header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import robotImage from "../assets/robot.png";
+import { useUserStore } from "../stores/user";
+import { useRouter } from "vue-router";
+
+const userStore = useUserStore();
+const router = useRouter();
+
+const logout = () => {};
+</script>
+
+<template>
+  <div
+    class="py-4 px-6 bg-gray-800 shadow-md flex justify-between items-center"
+  >
+    <img :src="robotImage" alt="Chat AI" class="w-8 h-8" />
+    <h1 class="text-xl font-semibold">Chat AI</h1>
+    <button @click="logout" class="text-gray-400 hover:text-white">
+      Logout
+    </button>
+  </div>
+</template>
